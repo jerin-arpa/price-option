70050c109b44e13cab0d75b538802a15f9a0feb6
jerin-arpa <ismatjerrin3@gmail.com>
2023-09-19 03:23:12 +0600

    Add react load spinner

--- src/component/Phones/Phones.jsx
+++ src/component/Phones/Phones.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { Grid } from 'react-loader-spinner'
+
 
 const Phones = () => {
 
     const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
+
 
     useEffect(() => {
         /* 
@@ -24,21 +28,39 @@ const Phones = () => {
                     return obj;
                 })
                 setPhones(phonesWithFakeData);
+                setLoading(false);
             })
     }, []);
 
 
 
     return (
-        <div className="mt-10">
-            <h2 className="text-3xl font-bold mb-10">Phones: {phones.length}</h2>
+        <div>
+            {
+                loading && <div className="flex justify-center my-5">
+                    <Grid
+                        height="80"
+                        width="80"
+                        color="#4fa94d"
+                        ariaLabel="grid-loading"
+                        radius="12.5"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                        visible={true}
+                    />
+                </div>
+            }
 
-            <BarChart width={1200} height={400} data={phones}>
-                <Bar dataKey="price" fill="#8884d8" />
-                <XAxis dataKey="name"></XAxis>
-                <YAxis></YAxis>
-                <Tooltip></Tooltip>
-            </BarChart>
+            <div className="mt-10">
+                <h2 className="text-3xl font-bold mb-10">Phones: {phones.length}</h2>
+
+                <BarChart width={1200} height={400} data={phones}>
+                    <Bar dataKey="price" fill="#8884d8" />
+                    <XAxis dataKey="name"></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                </BarChart>
+            </div>
         </div>
     );
 };
